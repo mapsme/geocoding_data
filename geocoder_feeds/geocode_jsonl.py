8b64081fb521d35c006f7ab5c01074e42f8fb73b
Ilya Zverev <zverik@textual.ru>
2018-08-06 16:50:21 +0300

    Add int_names to regions feed

--- geocoder_feeds/geocode_jsonl.py
+++ geocoder_feeds/geocode_jsonl.py
@@ -13,26 +13,33 @@ def init_geocoder(database):
 def geocode(data):
     global cursor
     coords = data['geometry']['coordinates']
-    cursor.execute('''select level, name, tags->'addr:street', tags->'addr:housenumber'
+    cursor.execute('''select level, name, coalesce(tags->'name:en', tags->'int_name', name),
+                   tags->'addr:street', tags->'addr:housenumber'
                    from osm_polygon p join osm_polygon_geom g using (osm_id)
                    where ST_Intersects(g.way, ST_SetSRID(ST_MakePoint(%s, %s), 4326))
                    ''', (coords[0], coords[1]))
     props = data['properties']['address']
+    props_int = props.copy()
     for row in cursor:
         if row[0] == 'building':
-            if 'building' not in props and row[3]:
-                props['building'] = row[3]
-                if row[2]:
-                    props['street'] = row[2]
+            if 'building' not in props and row[4]:
+                props['building'] = row[4]
+                props_int['building'] = row[4]
+                if row[3]:
+                    props['street'] = row[3]
+                    if 'street' not in props_int:
+                        props_int['street'] = row[3]
         else:
             props[row[0]] = row[1]
+            props_int[row[0]] = row[2]
+    data['properties']['address_en'] = props_int
     return data
 
 
 def print_data(data):
     did = data['properties']['id']
     del data['properties']['id']
-    options.output.write(did + ' ' + json.dumps(data, ensure_ascii=False) + '\n')
+    options.output.write(str(did) + ' ' + json.dumps(data, ensure_ascii=False) + '\n')
 
 
 if __name__ == '__main__':
--- geocoder_feeds/regions_feed.py
+++ geocoder_feeds/regions_feed.py
@@ -28,22 +28,28 @@ def osmid_to_rid(osm_id):
 
 if __name__ == '__main__':
     cursor = psycopg2.connect('dbname=borders').cursor()
-    cursor.execute('''select osm_id, level, name, rank, ST_X(centroid), ST_Y(centroid)
-                   from osm_polygon where rank <= 14 and name is not null order by rank''')
+    cursor.execute('''select osm_id, level, name,
+                   coalesce(tags->'name:en', tags->'int_name', tags->'name:es'),
+                   rank, ST_X(centroid), ST_Y(centroid)
+                   from osm_polygon where rank <= 14 and name is not null order by rank limit 1000''')
 
     for region in cursor.fetchall():
-        osm_id, level, name, rank, lon, lat = region
-        cursor.execute('''select level, osm_id, name
+        osm_id, level, name, int_name, rank, lon, lat = region
+        cursor.execute('''select level, osm_id, name,
+                       coalesce(tags->'name:en', tags->'int_name', tags->'name:es')
                        from osm_polygon p join osm_polygon_geom g using (osm_id)
                        where ST_Intersects(g.way, ST_SetSRID(ST_MakePoint(%s, %s), 4326))
                        and rank < %s''', (lon, lat, rank))
         addr = {level: name}
+        addr_en = {level: int_name}
         for row in cursor:
             addr[row[0]] = row[2]
+            addr_en[row[0]] = row[3]
             addr[row[0]+'_id'] = osmid_to_rid(row[1])
+            addr_en[row[0]+'_id'] = osmid_to_rid(row[1])
         feature = {
             'type': 'Feature',
             'geometry': {'type': 'Point', 'coordinates': [lon, lat]},
-            'properties': {'name': name, 'rank': rank, 'address': addr}
+            'properties': {'name': name, 'rank': rank, 'address': addr, 'address_en': addr_en}
         }
         print(str(osmid_to_rid(osm_id)) + ' ' + json.dumps(feature, ensure_ascii=False))
